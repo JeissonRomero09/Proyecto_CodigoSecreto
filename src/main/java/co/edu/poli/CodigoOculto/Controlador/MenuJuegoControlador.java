3f7e1abf912519993b4850fd12cde7d7ababf2f2
JiReMr <119068170+JiReMr@users.noreply.github.com>
2026-04-16 13:14:24 -0500

    Cambios

--- src/main/java/co/edu/poli/CodigoOculto/Controlador/MenuJuegoControlador.java
+++ src/main/java/co/edu/poli/CodigoOculto/Controlador/MenuJuegoControlador.java
@@ -1,14 +1,14 @@
 package co.edu.poli.CodigoOculto.Controlador;
 
-import  co.edu.poli.CodigoOculto.Modelo.Jugador;
+import co.edu.poli.CodigoOculto.Modelo.Jugador;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 
 public class MenuJuegoControlador {
 
@@ -20,19 +20,25 @@ public class MenuJuegoControlador {
     @FXML
     private Text textoId;
 
-    //  Recibe el jugador desde el menú principal
+    // =========================
+    // RECIBIR JUGADOR
+    // =========================
     public void setJugador(Jugador jugador) {
         this.jugador = jugador;
         actualizarTexto();
     }
 
-    //  Se ejecuta al cargar la vista
+    // =========================
+    // INIT
+    // =========================
     @FXML
     public void initialize() {
         actualizarTexto();
     }
 
-    // Mostrar datos del jugador
+    // =========================
+    // MOSTRAR DATOS
+    // =========================
     private void actualizarTexto() {
         if (jugador != null) {
 
@@ -46,17 +52,19 @@ public class MenuJuegoControlador {
         }
     }
 
-    //  BOTÓN JUGAR  : IR A PantallaJuego.fxml
+    // =========================
+    // BOTÓN: IR A PANTALLA JUEGO
+    // =========================
     @FXML
     public void irAPantallaJuego(ActionEvent event) {
         try {
+
             FXMLLoader loader = new FXMLLoader(
                 getClass().getResource("/co/edu/poli/CodigoOculto/Vista/PantallaJuego.fxml")
             );
 
             Parent root = loader.load();
 
-            // 🔥 PASAR JUGADOR AL JUEGO
             PantallaJuegoControlador controller = loader.getController();
             controller.setJugador(jugador);
 
@@ -69,10 +77,13 @@ public class MenuJuegoControlador {
         }
     }
 
-    //  CERRAR SESIÓN
+    // =========================
+    // BOTÓN: CERRAR SESIÓN
+    // =========================
     @FXML
     public void cerrarSesion(ActionEvent event) {
         try {
+
             Parent root = FXMLLoader.load(
                 getClass().getResource("/co/edu/poli/CodigoOculto/Vista/MenuPrincipal.fxml")
             );
@@ -86,7 +97,9 @@ public class MenuJuegoControlador {
         }
     }
 
-    // obtener jugador
+    // =========================
+    // GETTER
+    // =========================
     public Jugador getJugador() {
         return jugador;
     }
